e9503c0b73759f22116031089ff81c49d1c6c4ee
nassifdu <dudugrtuio@gmail.com>
2024-12-05 14:09:52 -0300

    fixing things

--- organize.py
+++ organize.py
@@ -1,3 +1,19 @@
 
 from pathlib import Path
 home = Path.home()
+
+from textual.app import App, ComposeResult
+from textual.widgets import Label, DirectoryTree
+from textual.containers import Horizontal, Vertical
+
+class FolderOrganizer(App):
+    CSS_PATH='style.tcss'
+    def compose(self) -> ComposeResult:
+        yield Horizontal(
+            Vertical(id='left-panel'),
+            DirectoryTree(home, id='main-tree'),
+            Vertical(id='right-panel')
+        )
+
+if __name__ == '__main__':
+    FolderOrganizer().run()
